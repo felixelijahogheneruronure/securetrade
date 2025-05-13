6038448398c60d72ded06900d1e3f0708336a86f
felixelijahogheneruronure <felixelijahogheneruronure@gmail.com>
2025-05-13 15:34:26 +0100

    Update SecureChat.jsx

--- src/components/messages/SecureChat.jsx
+++ src/components/messages/SecureChat.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import '/import './SecureChat.css';'; // We will move styles here
+import './SecureChat.css'; // We will move styles here
 
 const SecureChat = () => {
   useEffect(() => {
